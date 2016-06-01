08aeefd9088e9eda91b35263989145c36d030f74
Cheung, Wilson <wilson.cheung@cengage.com>
2016-06-01 16:40:21 -0700

    0.0.7

--- client/components/WorkflowWidget/component.jsx
+++ client/components/WorkflowWidget/component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import moment from 'moment';
 
 import './style.css';
 
 export default class Header extends React.Component {
 
     render() {
-        return (<span className="header">Workflow Widget</span>);
+        return (<span className="header">Workflow Widget ({moment().format('LLL')})</span>);
     }
 }
