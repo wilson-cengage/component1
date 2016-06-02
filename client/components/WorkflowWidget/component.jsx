fde401a66a614212dde33421c4f20c489bbade6a
Cheung, Wilson <wilson.cheung@cengage.com>
2016-06-02 00:13:02 -0700

    0.0.8

--- client/components/WorkflowWidget/component.jsx
+++ client/components/WorkflowWidget/component.jsx
@@ -6,6 +6,6 @@ import './style.css';
 export default class Header extends React.Component {
 
     render() {
-        return (<span className="header">Workflow Widget ({moment().format('LLL')})</span>);
+        return (<span className="header">Toolbar Widget ({moment().format('LLL')})</span>);
     }
 }
